0a1d1eb2f96b2cbe891a5b87a7ad29d9cdcae33c
Sachi Angle <sachiangle@gmail.com>
2021-03-18 06:03:56 +0530

    landing page and home page complete

--- frontend/ll_app/src/App.js
+++ frontend/ll_app/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form class="box">
+          <p class="title is-1 is-spaced">live locally</p>
+          <p class="subtitle is-5">sign in to connect with a local at your destination</p>
+          <div class="field">
+            <div class="control">
+              <input class="input" type="email" placeholder="e.g. sachi@example.com"/>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <input class="input" type="password" placeholder="********"/>
+            </div>
+          </div>
+          <button class="button is-dark">Log in</button>
+          <button class="button is-light">Sign up</button>
+        </form>
       </header>
     </div>
   );
